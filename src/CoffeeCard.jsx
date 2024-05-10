4a572adaa3a6eb6c91a879e83c072f1b5cb78e08
Nate Sobeck <nsobeck910@gmail.com>
2024-05-10 13:48:17 -0400

    adjust styling for coffee menu and add CoffeeCard component

---
+++ src/CoffeeCard.jsx
@@ -0,0 +1,9 @@
+const CoffeeCard = () => {
+  return (  
+    <div className="coffee-card">
+      <h1>This is a coffee card</h1>
+    </div>
+  )
+}
+
+export default CoffeeCard
--- src/CoffeeMenu.jsx
+++ src/CoffeeMenu.jsx
@@ -1,16 +1,23 @@
+// components
+import CoffeeCard from "./CoffeeCard";
+
+useEffect(() => {
+  
+}, []);
+
 const CoffeeMenu = () => {
   return (  
     <div>
       <section className="menu-header">
         <h1 className="header-text">Our Collection</h1>
-        <p>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
+        <p className="text">Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
         <div className="btn-container">
           <button>All Products</button>
           <button>Available Now</button>
         </div>
       </section>
       <section className="coffee-list">
-        <h1>map through coffees here</h1>
+        <CoffeeCard />
       </section>
     </div>
   )
